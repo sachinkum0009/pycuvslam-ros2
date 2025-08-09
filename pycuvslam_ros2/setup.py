fdc6542b32409c4251fec191b4eeb52ba70deeb3
Sachin Kumar <sachinkum123567@gmail.com>
2025-08-10 01:38:31 +0200

    ci added to build

--- pycuvslam_ros2/setup.py
+++ pycuvslam_ros2/setup.py
@@ -4,7 +4,7 @@ package_name = 'pycuvslam_ros2'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
